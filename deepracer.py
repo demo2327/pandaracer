acd6717ec329f9a335db025045e7afd55a6979d4
MillerA <adam.miller@broadridge.com>
2019-12-07 14:41:25 -0800

    PandaRacer-0-0-8

--- deepracer.py
+++ deepracer.py
@@ -8,28 +8,28 @@ def reward_function(params):
     is_left = params['is_left_of_center']
     percent_track_complete = params['progress']+1
     steps = params['steps']+1
-
+    
     # create some parameters for us to use
     steps_to_complete = 500
     percent_steps_used = steps/steps_to_complete
 
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    center_of_lane = 0.25 * track_width
+    wall = 0.5 * track_width
 
     # Give higher reward if the agent is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward *= 1
-    elif distance_from_center <= marker_2:
         reward *= 5
-    elif distance_from_center <= marker_3:
+    elif distance_from_center <= center_of_lane:
+        reward *= 3
+    elif distance_from_center <= wall:
         reward *= 1
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-6  # likely crashed/ close to off track
     # make the reward based on the ratio of track complete vs steps complete
 
-    reward *= percent_track_complete/percent_steps_used
+    # reward *= percent_track_complete/percent_steps_used
 
     # reward for being on the left side of the track
 
@@ -38,11 +38,23 @@ def reward_function(params):
     else:
         reward *= 2
 
+    
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 1
 
     # Penalize reward if the agent is steering too much
     if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.7
+        reward *= 0.8
+    
+    # print out some values so that we can see what they are
+
+    print("The number of steps is {}".format(steps))
+    print("The percent_steps_used is {}".format(percent_steps_used))
+    print("The completion bonus is {}".format(percent_track_complete/percent_steps_used))
+    print("the reward is {}".format(reward))
+    print("the current speed is {}".format(params['speed']))
+    print("the current progress is {}".format(params['progress']))
+    
+    
 
     return float(reward)
